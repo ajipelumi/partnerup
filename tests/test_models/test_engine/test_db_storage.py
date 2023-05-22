e7984c2fcb21262b6e96552cdf7635f2493755b6
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-05-22 19:50:37 +0100

    fix test errors

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -3,6 +3,7 @@ from models import storage
 from models.user import User
 from models.partner import Partner
 
+
 class Test_DBStorage(unittest.TestCase):
     def test_get(self):
         """ Tests method for getting an instance. """
