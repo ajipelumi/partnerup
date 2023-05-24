8d1e9fffb6618943ffd48af1f059bf1e9f634ff4
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-05-24 20:31:52 +0100

    made changes for profile callback

--- web_static/scripts/signup-main.js
+++ web_static/scripts/signup-main.js
@@ -21,7 +21,7 @@ $(document).ready(() => {
 
         $.ajax({
             type: 'POST',
-            url: 'http://127.0.0.1:5000/api/v1/users',
+            url: 'http://34.207.127.10:5000/api/v1/users',
             contentType: 'application/json',
             data: JSON.stringify(new_user),
             success: (response) => {
