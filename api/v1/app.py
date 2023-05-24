a328d6d32bed4e7b8d11fe07847067162613e47f
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-05-24 15:49:03 +0100

    modified ap app file

--- api/v1/app.py
+++ api/v1/app.py
@@ -25,7 +25,7 @@ def not_found_method(error):
 
 
 if __name__ == "__main__":
-    host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.environ.get("HBNB_API_PORT", 5000))
+    host = os.environ.get("PARTNERUP_API_HOST", "0.0.0.0")
+    port = int(os.environ.get("PARTNERUP_API_PORT", 5000))
 
     app.run(host=host, port=port, threaded=True)
