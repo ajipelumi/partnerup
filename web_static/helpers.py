79b7fc7572e2a6a30694086a5163da91b53f0003
root <root@ip-172-31-81-31.ec2.internal>
2023-05-30 21:11:42 +0000

    profile functionality complete

--- web_static/helpers.py
+++ web_static/helpers.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3
+import os
 import requests
+from urllib.parse import urlparse, urlunparse, parse_qs, urlencode
 
+
+access_token = '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'
 
 def get_commit_count(commits_url):
     """ Get the number of commits from a GitHub repository. """
-    response = requests.get(commits_url)
+    headers = {
+            'User-Agent': 'My Custom User-Agent',
+            'Authorization': f'Token {access_token}'
+            }
+    commits_url = commits_url[:commits_url.index("{/sha}")]
+    parsed_url = urlparse(commits_url)
+    query_params = parse_qs(parsed_url.query)
+    query_params['per_page'] = ['100']
+    encoded_query_params = urlencode(query_params, doseq=True)
+    updated_url = urlunparse(parsed_url._replace(query=encoded_query_params))
+    response = requests.get(updated_url, headers=headers)
     if response:
         commits = response.json()
         return len(commits)
--- web_static/profile.py
+++ web_static/profile.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 """ Starts a Flash Web Application """
 from flask import Flask, render_template, session
-from helpers import get_commit_count
+from web_static.helpers import get_commit_count
 from models import storage
 import requests
 import uuid
+import os
+
 
 app = Flask(__name__)
 app.secret_key = 'secret_key'
+access_token = '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'
 
 @app.teardown_appcontext
 def close_db(exception):
@@ -20,12 +23,16 @@ def profile_page():
     """ Create a particular user's page. """
     user = session.get('user')
     url = f'https://api.github.com/users/{user.get("username")}'
-    user_response = requests.get(url).json()
-    if user_response is None:
+    headers = {
+            'User-Agent': 'My Custom User-Agent',
+            'Authorization': f'Token {access_token}'
+            }
+    user_response = requests.get(url, headers=headers).json()
+    if user_response is None or 'repos_url' not in user_response:
         error_message = "Error: Failed to retrieve data from the GitHub."
         return render_template('error.html', error_message=error_message)
     
-    repo_response = requests.get(user_response.get('repos_url')).json()
+    repo_response = requests.get(user_response.get('repos_url'), headers=headers).json()
     if repo_response is None:
         error_message = "Error: Failed to retrieve data from the GitHub."
         return render_template('error.html', error_message=error_message)
@@ -33,7 +40,8 @@ def profile_page():
     return render_template('profile.html',
                            user_response=user_response,
                            repo_response=repo_response,
-                           cache_id=uuid.uuid4())
+                           cache_id=uuid.uuid4(),
+                           get_commit_count=get_commit_count)
 
 
 if __name__ == "__main__":
