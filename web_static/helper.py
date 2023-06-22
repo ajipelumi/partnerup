042c5383f9db94a43de4a50d56c81a867a2a7739
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-06-22 13:21:25 +0100

    refactored methods/functions for scalability

--- web_static/helper.py
+++ web_static/helper.py
@@ -6,101 +6,194 @@ import io
 import matplotlib.pyplot as plt
 
 
+# Load environment variables from .env file
 config = dotenv_values(".env")
+
+# Load GitHub access token from .env file
 access_token = config.get('ACCESS_TOKEN')
+
+# Create GitHub instance
 github = Github(access_token)
 
 
 def get_user_from_github(username):
     """ Get a particular user's data from GitHub. """
     try:
+        # Get user data from GitHub
         user = github.get_user(username)
+
+        # Return user raw data
         return user.raw_data
     except Exception:
+        # Return None if user does not exist
         return None
 
 
 def get_repos_from_github(username):
     """ Get all repositories for a particular user. """
     try:
+        # Get user data from GitHub
         user = github.get_user(username)
+
+        # Get all repositories for user
         repos = user.get_repos()
+
+        # Create a list of repository raw data
         repo_data = []
+
+        # Iterate through all repositories
         for repo in repos:
+
+            # Append repository raw data to list
             repo_data.append(repo.raw_data)
+
+        # Return list of repository raw data
         return repo_data
     except Exception:
+        # Return None if user does not exist
         return None
 
 
 def get_all_commits(username, repo, max_commits=30):
-    """ Get up to 'max_commits' commits for a particular user and repository. """
+    """ Get up to 'max_commits' commits for a particular user and repo. """
     try:
+        # Get repository data from GitHub
         repository = github.get_repo(f"{username}/{repo}")
+
+        # Get up to 'max_commits' commits for repository
         commits = repository.get_commits()[:max_commits]
+
+        # Create a list of commit raw data
         commit_data = []
+
+        # Iterate through all commits
         for commit in commits:
+
+            # Append commit raw data to list
             commit_data.append(commit.raw_data)
+
+        # Return list of commit raw data
         return commit_data
     except Exception:
+        # Return None if user or repository does not exist
         return None
 
 
 def get_total_commit_count(username, repo):
-    """ Get the total number of commits for a particular user and repository. """
+    """ Get the total number of commits for a particular user and repo. """
     try:
+        # Get repository data from GitHub
         repository = github.get_repo(f"{username}/{repo}")
+
+        # Get all commits for repository
         commits = repository.get_commits()
+
+        # Create a counter for total commits
         total_commit_count = 0
+
+        # Iterate through all commits
         for _ in commits:
+
+            # Increment total commit count
             total_commit_count += 1
+
+        # Return total commit count
         return total_commit_count
     except Exception:
+        # Return None if user or repository does not exist
         return None
 
 
 def commits_during_night(commits):
     """ Check if commits are made at night. """
+    # Iterate through all commits
     for commit in commits:
+
+        # Get commit time
         commit_time = commit.get('commit').get('author').get('date')
+
+        # Get commit hour
         commit_hour = int(commit_time[11:13])
+
+        # Check if commit is made at night
         if commit_hour < 6 or commit_hour > 18:
+
+            # Return True if commit is made at night
             return True
+
+    # Return False if no commits are made at night
     return False
 
 
 def process_commit_by_date(commits):
     """ Process commit counts by date. """
+    # Create a dictionary of commit counts by date
     commit_counts = {}
+
+    # Iterate through all commits
     for commit in commits:
+
+        # Get commit date
         date = commit['commit']['author']['date'][:10]
+
+        # Check if date is in dictionary
         if date not in commit_counts:
+
+            # Add date to dictionary if not in dictionary
             commit_counts[date] = 0
+
+        # Increment commit count for date if date is in dictionary
         commit_counts[date] += 1
+
+    # Return dictionary of commit counts by date
     return commit_counts
 
 
 def plot_image(user, user_commit_data, partner_commit_data, selected_partner):
     """ Visualize commit history with matplotlib. """
+    # Create a figure
     plt.figure(figsize=(20, 10))
+
+    # Get user commit counts by date
     user_commit_counts = process_commit_by_date(user_commit_data)
+
+    # Get user commit dates and values
     user_commit_dates = list(user_commit_counts.keys())
     user_commit_values = list(user_commit_counts.values())
+
+    # Plot user commit values
     plt.plot(user_commit_dates, user_commit_values,
              label=f'{user.get("username")}')
 
+    # Get partner commit counts by date
     partner_commit_counts = process_commit_by_date(partner_commit_data)
+
+    # Get partner commit dates and values
     partner_commit_dates = list(partner_commit_counts.keys())
     partner_commit_values = list(partner_commit_counts.values())
+
+    # Plot partner commit values
     plt.plot(partner_commit_dates, partner_commit_values,
              label=f'{selected_partner.get("username")}')
+
+    # Set legend, x-axis label, y-axis label, title, and grid
     plt.legend()
     plt.xlabel('Date')
     plt.ylabel('Number of Commits')
     plt.title('Commits per day')
     plt.grid(True)
+
+    # Create a buffer
     buffer = io.BytesIO()
+
+    # Save plot to buffer as png
     plt.savefig(buffer, format='png')
+
+    # Seek to beginning of buffer
     buffer.seek(0)
+
+    # Encode buffer as base64
     plot_bytes = base64.b64encode(buffer.read()).decode('utf-8')
+
+    # Return base64 encoded plot
     return plot_bytes
--- web_static/profile.py
+++ web_static/profile.py
@@ -9,33 +9,43 @@ import uuid
 from web_static.helper import *
 
 
+# Create Flask app
 app = Flask(__name__)
+
+# Set secret key for session
 app.secret_key = 'secret_key'
 
 
 @app.teardown_appcontext
 def close_db(exception):
     """ Remove the current SQLAlchemy Session. """
+    # Close current SQLAlchemy Session
     storage.close()
 
 
 @app.route('/profile', strict_slashes=False)
 def profile_page():
     """ Create a particular user's page. """
+    # Get user data from session
     user = session.get('user')
+
+    # Get username and cohort number from user data
     username = user.get('username')
     cohort_number = user.get('cohort_number')
 
+    # Get user data from GitHub
     user_response = get_user_from_github(username)
     if user_response is None or 'repos_url' not in user_response:
         error_message = "Error: Failed to retrieve data from GitHub."
         return render_template('error.html', error_message=error_message)
 
+    # Get user repositories from GitHub
     repo_response = get_repos_from_github(username)
     if repo_response is None:
         error_message = "Error: Failed to retrieve data from GitHub."
         return render_template('error.html', error_message=error_message)
 
+    # Render profile page
     return render_template('profile.html',
                            cohort_number=cohort_number,
                            user_response=user_response,
@@ -46,58 +56,52 @@ def profile_page():
 @app.route('/match', strict_slashes=False)
 def match_page():
     """ Match users based on their GitHub repositories. """
-    project = request.args.get('project')
-    repo = request.args.get('repo')
-    time = request.args.get('time')
+    # Get data from request
+    project, repo, time = get_request_data()
 
+    # Set matching partners to empty list
     matching_partners = []
 
-    user = session.get('user')
-    username = user.get('username')
-    cohort_number = int(user.get('cohort_number'))
+    # Get user data from session
+    user, username, cohort_number = get_user_info()
+
+    # Get user commits from GitHub
     user_commit_data = get_all_commits(username, repo)
     if user_commit_data is None:
         error_message = "Error: Failed to retrieve data from GitHub."
         return render_template('error.html', error_message=error_message)
+    # Get user commit count
     user_commit_count = get_total_commit_count(username, repo)
 
-    all_partners = storage.all(Partner).values()
+    all_partners = storage.all(Partner).values()  # list of partner objects
 
+    # Iterate through all matching partners
     while len(matching_partners) == 0:
+        # Get three random partners
         random_partners = random.sample(list(all_partners), 3)
 
-        for partner in random_partners:
+        for partner in random_partners:  # Iterate through all random partners
             partner = partner.to_dict()
 
-            if username == partner.get('username'):
+            if username == partner.get('username'):  # Skip if partner is user
                 continue
 
+            # Skip if partner is not in the same cohort
             if cohort_number != partner.get('cohort_number'):
                 continue
 
             partner_commit_data = get_all_commits(partner.get('username'), repo)
             if partner_commit_data is None:
-                error_message = "Error: Failed to retrieve data from GitHub."
-                return render_template('error.html', error_message=error_message)
+                err = "Error: Failed to retrieve data from GitHub."
+                return render_template('error.html', error_message=err)
+            # Get partner commit count
             partner_commit_count = get_total_commit_count(partner.get('username'), repo)
 
-            if time == 'night' and commits_during_night(partner_commit_data):
-                matching_partners.append({
-                    'username': partner.get('username'),
-                    'id': partner.get('id'),
-                    'email': partner.get('email'),
-                    'commit_count': partner_commit_count,
-                })
-            else:
-                matching_partners.append({
-                    'username': partner.get('username'),
-                    'id': partner.get('id'),
-                    'email': partner.get('email'),
-                    'commit_count': partner_commit_count,
-                })
+            # Check if partner commits during the day or night
+            commit_period(time, matching_partners, partner, partner_commit_data, partner_commit_count)
 
-    sorted_partners = sorted(matching_partners, key=lambda partner: abs(partner['commit_count'] - user_commit_count))
-    selected_partner = sorted_partners[0]
+    # Select best partner
+    selected_partner = select_best_partner(matching_partners, user_commit_count)
 
     partner_commit_data = get_all_commits(selected_partner.get('username'), repo)
     if partner_commit_data is None:
@@ -109,14 +113,14 @@ def match_page():
         error_message = "Error: Failed to retrieve data from GitHub."
         return render_template('error.html', error_message=error_message)
 
+    # Get plot image URL
     plot_image_url = plot_image(user, user_commit_data, partner_commit_data, selected_partner)
     email = selected_partner.get('email')
 
-    current_user = storage.get(User, user.get('id'))
-    selected_partner = storage.get(Partner, selected_partner.get('id'))
-    current_user.partners.append(selected_partner)
-    storage.save()
+    # Save user and partner to database
+    save_user_partner(user, selected_partner)
 
+    # Render match page
     return render_template('match.html',
                            project=project,
                            partner_response=partner_response,
@@ -126,19 +130,79 @@ def match_page():
                            )
 
 
+def get_request_data():
+    """ Get data from request. """
+    project = request.args.get('project')
+    repo = request.args.get('repo')
+    time = request.args.get('time')
+    return project, repo, time
+
+
+def get_user_info():
+    """ Get user data from session. """
+    user = session.get('user')
+    username = user.get('username')
+    cohort_number = int(user.get('cohort_number'))
+    return user, username, cohort_number
+
+
+def commit_period(time, matching_partners, partner, partner_commit_data, partner_commit_count):
+    """ Check if partner commits during the day or night. """
+    if time == 'night' and commits_during_night(partner_commit_data):
+        matching_partners.append({
+                    'username': partner.get('username'),
+                    'id': partner.get('id'),
+                    'email': partner.get('email'),
+                    'commit_count': partner_commit_count,
+                })
+    else:
+        matching_partners.append({
+                    'username': partner.get('username'),
+                    'id': partner.get('id'),
+                    'email': partner.get('email'),
+                    'commit_count': partner_commit_count,
+                })
+
+
+def select_best_partner(matching_partners, user_commit_count):
+    """ Select the best partner from a list of matching partners. """
+    # Sort matching partners by commit count
+    sorted_partners = sorted(matching_partners, key=lambda partner: abs(partner['commit_count'] - user_commit_count))
+
+    # Select partner with the least difference in commit count
+    selected_partner = sorted_partners[0]
+
+    # Return selected partner
+    return selected_partner
+
+
+def save_user_partner(user, selected_partner):
+    """ Save user and partner to database. """
+    current_user = storage.get(User, user.get('id'))
+    selected_partner = storage.get(Partner, selected_partner.get('id'))
+    current_user.partners.append(selected_partner)
+    storage.save()
+
+
 @app.route('/previous-matches', strict_slashes=False)
 def previous_matches_page():
     """ Display a user's previous matches. """
+    # Get user data from session
     user = session.get('user')
+
+    # Set up list of partners
     partners = []
 
+    # Get current user and all partners
     current_user = storage.get(User, user.get('id'))
     all_partners = current_user.partners
 
+    # Iterate through all partners and add to list
     for partner in all_partners:
         partner = storage.get(Partner, partner.id)
         partners.append(partner.to_dict())
 
+    # Render previous matches page
     return render_template('previous-matches.html',
                            partners=partners,
                            cache_id=uuid.uuid4())
